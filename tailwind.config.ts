a0812108d20e11e751ff2b96ddf6054c9eef9c42
Noah Zoschke <noah@mixable.net>
2024-12-06 19:36:10 -0800

    organize imports

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from "tailwindcss";
 import daisyui from "daisyui";
+import type { Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
